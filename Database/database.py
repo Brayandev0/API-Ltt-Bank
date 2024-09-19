8aa0d0d92b4d7a04ed1d81cf8d66bd576291fe29
Brayan Dev <onoratogamer@gmail.com>
2024-09-18 21:22:49 -0300

    atualizacao api
    Foi adicionado a verificacao das chaves api no banco de dados

--- Database/database.py
+++ Database/database.py
@@ -17,8 +17,16 @@ class Usuario(Model):
     cpf = CharField(15)
     email = CharField(26)
     senha = TextField()
+    saldo = IntegerField()
     class Meta:
         database = db
 
-
+class Chaves(Model):
+    nome = CharField(30)
+    key = TextField(220)
+    class Meta:
+        database = db
 db.create_tables([Usuario])
+
+db.create_tables([Chaves])
+
--- create.py
+++ create.py
@@ -1,10 +1,10 @@
 from flask import Flask,request,Blueprint, render_template
-import http
-
-from Database.database import Usuario
+from Database.database import Usuario,Chaves
 app = Blueprint('Create',__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET'])
+
+# Cria a pagina create 
 def create():
     nome = request.args.get('nome')
     key_api = request.args.get('key')
@@ -13,35 +13,47 @@ def create():
     cpf = request.args.get('cpf')
     email = request.args.get('email')
     senha = request.args.get('senha')
-    valores = [data_nascimento,nome_da_mae,cpf,email,senha,nome]
-    data = verificar_valores(valores,key_api)
 
-    if data and tratar_valores(valores,cpf):
-        Usuario.create(nome=nome,data_de_nascimento=data_nascimento,nome_da_mae=nome_da_mae,cpf=cpf,email=email,senha=senha)
-        return 'deu'
-    elif data == None:
+# Verifica os valores informados 
+    data = verificar_valores([data_nascimento,nome_da_mae,cpf,email,senha,nome])
+    valida_api = verificar_chave_api(key_api)
+
+# verifica os dados e a api 
+    if data and valida_api and verificar_user_existente(cpf,email) == 'nao':
+            Usuario.create(nome=nome,data_de_nascimento=data_nascimento,nome_da_mae=nome_da_mae,cpf=cpf,email=email,senha=senha,saldo=0)
+            return 'deu'
+
+    elif valida_api is None:
+#   retorna paginas de erro 
         return render_template('Erros_pagina_create/chave_api_invalida.html')
+    elif verificar_user_existente(cpf,email) == 'existe':
+        return 'aa'
     else:
         return render_template('Erros_pagina_create/campos_vazios_invalidos.html')
     
-def verificar_valores(valores : list, key : str):
+# verifica e trata os valores recebidos
+def verificar_valores(valores : list):
     for valor in valores:
         if valor == None or not valor:
             return False
+        elif len(valor) < 3:
+            return False
+    return True
+
+# consulta e verifica as chaves api 
+def verificar_chave_api(key : str):
     try:
-        if int(key) in [0,934,43,12,12]:
-            return True
+        Chaves.select().where(Chaves.key == key).get()
+        return True
     except:
         return None
-    else:
-        return None
- 
-def tratar_valores(lista : list, cpf : str):
-    for valor in lista:
-        if len(valor) < 3:
-            return False
-        try:
-            int(cpf) + 3
-        except :
-            return False
-        return True
+
+# verifica se o cpf e o email estao cadastrados 
+
+def verificar_user_existente(cpf,email):
+    try:
+        Usuario.select().where(Usuario.cpf == cpf).get()
+        Usuario.select().where(Usuario.email == email).get()
+        return f'existe'
+    except Exception as e :
+        return 'nao'
